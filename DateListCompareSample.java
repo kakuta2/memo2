cd179f31b488a8025714ecf6c1b4da9b5c721658
kakuta2 <kakuchi77@gmail.com>
2021-12-11 09:14:44 +0900

    コメント外し

--- DateListCompareSample.java
+++ DateListCompareSample.java
@@ -3,15 +3,20 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 
+ * 
+ * 
+ */
 public class DateListCompareSample{
     public static void main(String[] args){
 
         List<Date> list = new ArrayList<Date>();
-        //list.add(Date.valueOf("2021-03-01"));
-        //list.add(Date.valueOf("2021-04-01"));
-        //list.add(Date.valueOf("2021-03-02"));
-        //list.add(Date.valueOf("2021-03-03"));
-        //list.add(Date.valueOf("2021-03-04"));
+        list.add(Date.valueOf("2021-03-01"));
+        list.add(Date.valueOf("2021-04-01"));
+        list.add(Date.valueOf("2021-03-02"));
+        list.add(Date.valueOf("2021-03-03"));
+        list.add(Date.valueOf("2021-03-04"));
         list.add(Date.valueOf("2021-03-05"));
         list.add(Date.valueOf("2021-03-07"));
         list.add(Date.valueOf("2021-03-06"));
@@ -21,8 +26,5 @@ public class DateListCompareSample{
         System.out.println(Collections.min(list));
         System.out.println(Collections.max(list));
         
-        
-
-
     }
 }
