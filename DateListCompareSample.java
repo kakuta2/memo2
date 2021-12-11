81e0802adb0fe8a3fa9d7d7e4dde50b41a6d434a
kakuta2 <kakuchi77@gmail.com>
2021-12-11 18:54:18 +0900

    デバッグログを削除

--- DateListCompareSample.java
+++ DateListCompareSample.java
@@ -23,8 +23,9 @@ public class DateListCompareSample{
         list.add(Date.valueOf("2021-04-01"));
         list.add(Date.valueOf("2021-04-02"));
         list.add(Date.valueOf("2021-04-03"));
-        System.out.println(Collections.min(list));
-        System.out.println(Collections.max(list));
-        
+        //デバッグ用
+        //System.out.println(Collections.min(list));
+        //System.out.println(Collections.max(list));
+
     }
 }
