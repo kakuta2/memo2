036187fb83604753e7c4d84a497c06a92b857fe9
kakuta2 <kakuchi77@gmail.com>
2021-12-11 09:20:20 +0900

    ４月の位置を変更、１行追加

--- DateListCompareSample.java
+++ DateListCompareSample.java
@@ -6,14 +6,12 @@ import java.util.List;
 /**
  * 
  * 
- * 
  */
 public class DateListCompareSample{
     public static void main(String[] args){
 
         List<Date> list = new ArrayList<Date>();
         list.add(Date.valueOf("2021-03-01"));
-        list.add(Date.valueOf("2021-04-01"));
         list.add(Date.valueOf("2021-03-02"));
         list.add(Date.valueOf("2021-03-03"));
         list.add(Date.valueOf("2021-03-04"));
@@ -22,6 +20,8 @@ public class DateListCompareSample{
         list.add(Date.valueOf("2021-03-06"));
         list.add(Date.valueOf("2021-03-09"));
         list.add(Date.valueOf("2021-03-08"));
+        list.add(Date.valueOf("2021-04-01"));
+        list.add(Date.valueOf("2021-04-02"));
 
         System.out.println(Collections.min(list));
         System.out.println(Collections.max(list));
